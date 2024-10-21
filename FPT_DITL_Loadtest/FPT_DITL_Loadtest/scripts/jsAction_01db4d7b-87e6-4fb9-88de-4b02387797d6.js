98c45389b382f64f5af3009d4aad200db346194c
Nandlal Pal <Nandu.qk@gmail.com>
2024-10-21 13:11:12 +0000

    with HBD

---
+++ FPT_DITL_Loadtest/FPT_DITL_Loadtest/scripts/jsAction_01db4d7b-87e6-4fb9-88de-4b02387797d6.js
@@ -0,0 +1,7 @@
+﻿var myVar = context.variableManager.getValue("status");
+if (myVar !=200) {
+        context.fail("Status other than 200 Status=" +myVar);
+      //  logger.debug("Status="+myVar);
+}
+
+logger.debug("Status="+myVar);
